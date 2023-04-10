83c62c9fa499ca4befff8f425607c9c8addb5fa2
Andres Alban <bethesdas101@gmail.com>
2023-04-09 19:49:34 -0500

    renaming appName in capacitor config file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.expenses.application',
-  appName: 'expenses',
+  appName: 'Expenses EC',
   webDir: 'www',
   bundledWebRuntime: false
 };
