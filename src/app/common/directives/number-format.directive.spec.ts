99dfe1d687fdb09be6d78af278957088569b72c2
Andres Alban <bethesdas101@gmail.com>
2023-04-20 21:55:54 -0500

    fixing syntax error for test directive, not an actual valid test

--- src/app/common/directives/number-format.directive.spec.ts
+++ src/app/common/directives/number-format.directive.spec.ts
@@ -1,8 +1,11 @@
+import { ElementRef } from '@angular/core';
 import { NumberFormatDirective } from './number-format.directive';
 
 describe('NumberFormatDirective', () => {
   it('should create an instance', () => {
-    const directive = new NumberFormatDirective();
+    const mockInputElement = new HTMLInputElement();
+    const mockElementRef: ElementRef<any> = new ElementRef(mockInputElement)
+    const directive = new NumberFormatDirective(mockElementRef);
     expect(directive).toBeTruthy();
   });
 });
