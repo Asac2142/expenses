543deed0af0f69ecddb14b2ed140c74c795e8342
Andres Alban <bethesdas101@gmail.com>
2023-04-20 22:10:25 -0500

    adding filter for categories

--- src/app/home/category-modal/category-modal.component.ts
+++ src/app/home/category-modal/category-modal.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input, OnInit, inject } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
-import { Observable } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 import { Store } from '@ngrx/store';
 
 import { Category, TransactionType } from 'src/app/common/models/transaction.model';
@@ -20,14 +20,24 @@ export class CategoryModalComponent implements OnInit {
   @Input() transactionType!: TransactionType;
   private _modal = inject(ModalController);
   private store = inject(Store);
+  private _categories!: Category[];
   categories$!: Observable<Category[]>;
 
   ngOnInit(): void {
-    this.categories$ = this.store.select(TransactionSelectors.selectCategories(this.transactionType));
+    this.categories$ = this.store
+      .select(TransactionSelectors.selectCategories(this.transactionType))
+      .pipe(tap(categories => (this._categories = categories)));
   }
 
   onSelected(category: Category): void {
     this._modal.dismiss(category);
+  }
+
+  onSearchCategory(event: Event): void {
+    const value = (event.target as HTMLInputElement).value.toLowerCase() as string;
+    this.categories$ = of(this._categories).pipe(
+      map(categories => categories.filter(c => c.label.toLowerCase().indexOf(value) > -1))
+    );
   }
 
   onCancel(): void {
