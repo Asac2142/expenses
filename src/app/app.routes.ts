1e4ffc4834540200c2df42a951a6fa869cb3599b
Andres Alban <bethesdas101@gmail.com>
2023-05-31 16:21:03 -0500

    refactoring routes, adding menu bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,17 @@ import { Routes } from '@angular/router';
 
 const transactionsComponent = () => import('./pages/transactions/transactions.component').then(c => c.TransactionsComponent);
 const analitycsComponent = () => import('./pages/analytics/analytics.component').then(c => c.AnalyticsComponent);
+const homeComponent = () => import('./pages/home/home.page').then(c => c.HomePage);
 
 export const routes: Routes = [
   { path: '', redirectTo: 'transactions', pathMatch: 'full' },
-  { path: 'transactions', loadComponent: transactionsComponent },
-  { path: 'analytics', loadComponent: analitycsComponent },
+  {
+    path: '',
+    loadComponent: homeComponent,
+    children: [
+      { path: 'transactions', loadComponent: transactionsComponent },
+      { path: 'analytics', loadComponent: analitycsComponent }
+    ]
+  },
   { path: '**', redirectTo: 'transactions' }
 ];
