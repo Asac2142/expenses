e7b237e97727c3c8fe57df7993496bd03c08a77f
Andres Alban <bethesdas101@gmail.com>
2023-06-16 23:09:54 -0500

    rolling back

--- src/app/common/components/currency-modal/currency-modal.component.ts
+++ src/app/common/components/currency-modal/currency-modal.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, inject } from '@angular/core';
 import { IonicModule, ModalController } from '@ionic/angular';
 import { Store } from '@ngrx/store';
 import { Observable, map, of, tap } from 'rxjs';
-import { Capacitor } from '@capacitor/core'
 
 import { RootState } from '@store/index';
 import { CurrencieByCountry } from '../../models/country.model';
@@ -44,10 +43,6 @@ export class CurrencyModalComponent implements OnInit {
     this.currencyCountries$ = of(this._currencyInfo).pipe(
       map(countryInfo => countryInfo.filter(c => c.country.toLowerCase().indexOf(value) > -1))
     );
-  }
-
-  convertImageSource(source: string): string {
-    return Capacitor.convertFileSrc(source);
   }
 
   index(index: number): number {
